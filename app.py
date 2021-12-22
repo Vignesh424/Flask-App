305ba15339926825074c2d242701ac3e1612f73b
Vignesh Suresh <61021342+Vignesh424@users.noreply.github.com>
2021-12-22 18:30:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import *
+
+app = Flask(__name__)
+app.secret_key = "@345^"
+
+@app.route('/')
+def index():
+    flash("Please enter your name?")
+    return render_template("index.html")
+
+@app.route("/greet", methods=['POST', 'GET'])
+def greet():
+    flash("Hi " + str(request.form['name_input']) + ", great to see you!")
+    return render_template("index.html")
+
